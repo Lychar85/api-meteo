3bed82f071cd0350309d89a5178d4bfdf2724fc9
Lychar85 <63816497+Lychar85@users.noreply.github.com>
2020-12-17 11:11:19 +0100

    .

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ fetch(url).then((response) =>
         document.querySelector('#maxtempnum').innerHTML = data.main.temp_max + '°';
         document.querySelector('#maxtempnum2').innerHTML = data.main.temp_max + '°';
         document.querySelector('.ventnum').innerHTML = data.wind.speed;
-        document.querySelector('.humiditenum').innerHTML = data.clouds.all + '%';
-        document.querySelector('.pressionnum').innerHTML = data.main.pressure;
+        document.querySelector('.humiditenum').innerHTML = data.main.humidity + '%';
+        document.querySelector('.pressionnum').innerHTML = data.main.pressure + 'Mb';
         document.querySelector('.visibilitenum').innerHTML = data.visibility;
-    
+        //ajout icon
+        
     })
 );
 
@@ -31,4 +32,61 @@ document.querySelector('form').addEventListener('submit', function(e){
     apicall(ville);
 });
 
-apicall('nantes')
+apicall('nantes');
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+var heuress = document.querySelector('#clock');
+var date = document.querySelector('#date');
+
+var affichageHeure = function(){
+    // Déclaration des variables 
+    var today, annee, listeMois, mois, listeJours, jourNUmero, jourNom, heures, minutes, secondes, deuxChiffres;
+
+    // Récupére la date actuelle : 
+    today = new Date();
+
+    // Récupére l'année : 
+    annee = today.getFullYear();
+
+    //Récupére le mois : 
+    listeMois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+    mois = listeMois[today.getMonth()]; //getMonth() donne l'index 1 comme on est en Février, ce qui donne la valeur "Février" depuis notre liste
+
+    // Récupére le numéro du jour du mois : 
+    jourNUmero = today.getDate();
+
+    // Récupére le jour : 
+    listeJours = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+    jourNom = listeJours[today.getDay()]; 
+
+
+    //Affiche les heures, minutes et secondes toujours avec deux chiffres : 
+    deuxChiffres = function(element){
+        if(element < 10){
+            return element = "0" + element;
+        } else {
+            return element;
+        }
+    }
+
+    // Récupére les heures : 
+    heures = deuxChiffres(today.getHours());
+
+    // Récupére les minutes : 
+    minutes = deuxChiffres(today.getMinutes());
+
+    // Récupére les secondes : 
+    secondes = deuxChiffres(today.getSeconds());
+
+    //Affichage  HTML : 
+    heuress.textContent = heures + ":" + minutes + ":" + secondes;
+    date.textContent = jourNom + ", " + jourNUmero + " " + mois + " " + annee;
+
+    // Lancer la fonction affichage heure toutes les 1000 ms, soit toute les secondes : 
+    setTimeout(affichageHeure, 1000);
+}
+
+//Lancer la fonction une fois au début : 
+affichageHeure();
